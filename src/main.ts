d012f8824932aa057791ecb6fc5a16b294ceb9d4
emromerog <emromerog@unal.edu.co>
2024-03-01 17:47:12 -0500

    chore: reinstall and configure tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import './style.css'
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
-import './style.css'
 
 
 
--- src/views/EnronEmailView.vue
+++ src/views/EnronEmailView.vue
@@ -10,24 +10,28 @@
     />
   </div>
 
-  <table v-if="showTable" class="table-auto">
-  <thead class="bg-gray-100">
-    <tr>
-      <th>#</th>
-      <th>From</th>
-      <th>To</th>
-      <th>Subject</th>
-    </tr>
-  </thead>
-  <tbody class="divide-y divide-gray-200">
-    <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" v-for="(item, index) in searchResults" :key="index">
-      <td class="px-6 py-4 whitespace-nowrap">{{ index + 1 }}</td>
-      <td class="px-6 py-4 whitespace-nowrap">{{ item.from }}</td>
-      <td class="px-6 py-4 whitespace-nowrap">{{ item.to }}</td>
-      <td class="px-6 py-4 whitespace-nowrap">{{ item.subject }}</td>
-    </tr>
-  </tbody>
-</table>
+  <table v-if="showTable" class="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th>#</th>
+        <th>From</th>
+        <th>To</th>
+        <th>Subject</th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200">
+      <tr
+        class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+        v-for="(item, index) in searchResults"
+        :key="index"
+      >
+        <td scope="col" class="px-6 py-3">{{ index + 1 }}</td>
+        <td scope="col" class="px-6 py-3">{{ item.from }}</td>
+        <td scope="col" class="px-6 py-3">{{ item.to }}</td>
+        <td scope="col" class="px-6 py-3">{{ item.subject }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
@@ -36,31 +40,39 @@ import { useEnronEmailStore } from "../stores/EnronEmailStore.ts";
 
 const store = useEnronEmailStore();
 
-const searchTerm = ref(''); // Variable para almacenar el término de búsqueda
+const searchTerm = ref(""); // Variable para almacenar el término de búsqueda
 const searchResults = ref([]); // Variable para almacenar los resultados de la búsqueda
 const showTable = ref(false);
 
 const handleSearch = async () => {
   try {
     // Realizar la petición GET a la API
-    const response = await fetch(`http://localhost:8080/api/v1/emails/search/${searchTerm.value}`);
+    const response = await fetch(
+      `http://localhost:8080/api/v1/emails/search/${searchTerm.value}`
+    );
 
     // Verificar si la respuesta es exitosa (código 200)
     if (response.ok) {
       // Convertir la respuesta a JSON
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       // Actualizar los resultados de la búsqueda
       searchResults.value = data;
       showTable.value = searchResults.value.length > 0;
     } else {
       // Manejar errores en la respuesta
-      console.error('Error en la solicitud:', response.status, response.statusText);
+      console.error(
+        "Error en la solicitud:",
+        response.status,
+        response.statusText
+      );
     }
   } catch (error) {
     // Manejar errores en la petición
-    console.error('Se produjo un error al realizar la búsqueda:', error.message);
+    console.error(
+      "Se produjo un error al realizar la búsqueda:",
+      error.message
+    );
   }
 };
 </script>
-

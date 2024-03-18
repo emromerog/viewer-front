79533f049c50aeac46f0e98fa768cfba9e54f2ca
emromerog <emromerog@unal.edu.co>
2024-03-18 05:17:19 -0500

    feat: add content to vue table

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import App from "./App.vue";
 import router from "./router";
 
 // Vuetify
-import "vuetify/styles";
+//import "vuetify/styles";
+import "vuetify/dist/vuetify.min.css"
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
--- src/stores/EnronEmailStore.ts
+++ src/stores/EnronEmailStore.ts
@@ -1,59 +1,58 @@
 import { defineStore } from "pinia";
+import { Email } from "../models/Email.ts";
 
 const GETAllEmailsUrl = "http://localhost:8080/api/v1/emails";
-const POSTSearchEmailsUrl = "http://localhost:8080/api/v1/emails/search/";
+const GETSearchEmailsUrl = "http://localhost:8080/api/v1/emails/search/";
+const POSTSearchEmailsUrl = "http://localhost:8080/api/v1/emails/";
 
 export const useEnronEmailStore = defineStore("enronEmailStore", {
   state: () => ({
-    searchTerm: "",
-    emailsSearchResult: [],
-    emails: [],
+    emails: [] as Email[],
   }),
   actions: {
-    async handleSearchEmails() {
+    async searchEmails(term: string) {
       try {
-        const response = await fetch(
-          `http://localhost:8080/api/v1/emails/search/${this.searchTerm.value}`
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          // Actualizar los resultados de la búsqueda
-          this.searchResults = data;
-        } else {
-          // Manejar errores en la respuesta
-          console.error(
-            "Error en la solicitud:",
-            response.status,
-            response.statusText
-          );
+        const response = await fetch(GETSearchEmailsUrl + term);
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
         }
+        const data = await response.json();
+        this.emails = data as Email[];
       } catch (error) {
-        // Manejar errores en la petición
-        console.error(
-          "Se produjo un error al realizar la búsqueda:",
-          error.message
-        );
+        console.error("Error al obtener los emails:", error.message);
       }
     },
-    getAllEmails() {
-      fetch(GETAllEmailsUrl)
-        .then((response) => response.json())
-        //.then((data) => console.log(data));
-        .then((data) => {
-          this.emails.values = data;
-        });
+    async getAllEmails() {
+      try {
+        const response = await fetch(GETAllEmailsUrl);
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        this.emails = data as Email[];
+      } catch (error) {
+        console.error("Error al obtener los emails:", error.message);
+      }
     },
-    searchEmails() {
-      fetch(POSTSearchEmailsUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ page: 1, itemsPerPage: 25 }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+    async searchEmailsPOST(searchTerm: string) {
+      try {
+        const response = await fetch(POSTSearchEmailsUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ page: 1, itemsPerPage: 25, term: searchTerm }),
+          //body: JSON.stringify({ term: searchTerm }),
+        });
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log(data.items)
+        this.emails = data.items as Email[];
+      } catch (error) {
+        console.error("Error al obtener los emails:", error.message);
+      }
     },
   },
 });
